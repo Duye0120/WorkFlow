2d864695112cec26061a2c900128f9987242ed0a
Duye <1939034592@qq.com>
2022-09-08 16:19:54 +0800

    chore: :hammer: 修复编译报错问题

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import ArcoLayout from '../components/ArcoLayout'
 import navArray from '../components/ArcoLayout/utils/layoutNavArray'
-
+import React from 'react';
+if (typeof document === 'undefined') {
+  React.useLayoutEffect = React.useEffect;
+}
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ArcoLayout NavArray={navArray}>
