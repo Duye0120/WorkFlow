bee09f43271df1d43422fc8a6d7e0efe804715ea
Duye <1939034592@qq.com>
2022-09-13 17:38:29 +0800

    fix: 修复build报错问题

--- components/ReactMarkdown/index.tsx
+++ components/ReactMarkdown/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Markdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { darcula as codeStyle } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 
 interface CodeBlockProps {
@@ -17,10 +17,11 @@ const ReactMarkdown = (props: CodeBlockProps) => {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         code({ node, inline, className, children, ...props }) {
           const match = /language-(\w+)/.exec(className || '');
+          // @ts-ignore
           return !inline && match ? (
             <SyntaxHighlighter
               language={match[1]}
-              style={codeStyle}
+              style={oneLight}
               PreTag='div'
               {...props}
             >
