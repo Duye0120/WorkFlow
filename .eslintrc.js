871d3ccf52abf2331d297069e2fb9f216e70cd7d
Duye <1939034592@qq.com>
2022-09-13 17:59:25 +0800

    Revert "Update .eslintrc.js"
    This reverts commit 94439ad13888348b97abfff2dff3d4c678f97477.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,5 @@ module.exports = {
     'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended',
     'prettier'
-  ],
-  'rules': {
-    'react/no-unescaped-entities': 'off',
-    '@next/next/no-page-custom-font': 'off'
-  }
-};
+  ]
+}
