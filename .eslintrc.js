94439ad13888348b97abfff2dff3d4c678f97477
Duye <1939034592@qq.com>
2022-09-13 17:54:22 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,9 @@ module.exports = {
     'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended',
     'prettier'
-  ]
-}
+  ],
+  'rules': {
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-page-custom-font': 'off'
+  }
+};
